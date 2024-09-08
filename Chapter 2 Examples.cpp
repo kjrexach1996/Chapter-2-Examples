ec9992f017903dc11e81bd2cbc245ef923de3295
Kevin Rexach <kaiorex@hotmail.com>
2024-09-08 14:05:31 -0400

    The original code was replaced with the code from Example 2, and all comments afterwards have been removed.

--- Chapter 2 Examples.cpp
+++ Chapter 2 Examples.cpp
@@ -1,20 +1,19 @@
-// Chapter 2 Examples.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// This program calculates and displays the discounted value of an item.
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	double regularPrice, percentDiscount, discountAmount,
+		reducedPrice;
+	regularPrice = 59.95;
+	percentDiscount = 0.2;
+	discountAmount = regularPrice * percentDiscount;
+	reducedPrice = regularPrice - discountAmount;
+	cout << "The discount amount is $" << discountAmount << endl;
+	cout << "The discounted price is $" << reducedPrice;
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
